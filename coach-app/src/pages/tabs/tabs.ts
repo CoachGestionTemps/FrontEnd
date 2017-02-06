70b399fe0a01b34a6b8db083fdc4d75d4cf4b741
Simon Lacoursiere <lacoursieresimon@gmail.com>
2017-02-06 01:18:46 -0500

    Installed moment and did some tests to see how it worked. Tried angular2-moment but locales aren't supported to I decided to remove it.

--- coach-app/src/pages/tabs/tabs.ts
+++ coach-app/src/pages/tabs/tabs.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 import { TodayPage } from '../today/today';
 import { WeekPage } from '../week/week';
 
+import moment from 'moment';
+import 'moment/src/locale/fr-ca';
+import 'moment/src/locale/en-ca';
+
+
 @Component({
   templateUrl: 'tabs.html'
 })
@@ -13,6 +18,8 @@ export class TabsPage {
   tab2Root: any = WeekPage;
 
   constructor() {
-
+      // TODO Load this information from settings (either en-ca or fr-ca)
+      // Will also be used for the translate files in /i18n
+      moment.locale('en-ca'); 
   }
 }
