56a1310724150239d38dbbac55caa0e9f6d63ade
Simon Lacoursiere <lacoursieresimon@gmail.com>
2017-02-10 15:41:05 -0500

    Calendar is working, code is ugly but I'll clean it up next time!

--- coach-app/src/pages/today/today.ts
+++ coach-app/src/pages/today/today.ts
@@ -12,10 +12,16 @@ import moment from 'moment';
 
 export class TodayPage {
   selectedDays: any;
+  month: any;
+  displayedMonth: any;
+  displayedYear: any;
+  moment: any;
+  today: any;
 
   constructor(public navCtrl: NavController) {
-    this.selectedDays = [moment(), moment().add(1, 'day'), moment().add(2, 'day')];
-
+    this.moment = moment;
+    this.today = moment();
+    this.setSelectedDay(null);
     // TODO : Load custom calendar using this.selectedDays[0]
   }
 
@@ -28,5 +34,78 @@ export class TodayPage {
       return "tomorrow";
     }
     return "";
+  }
+
+  setSelectedDay(day) {
+    if (day){
+      this.selectedDays = [moment(day), moment(day).add(1, 'day'), moment(day).add(2, 'day')];
+    } else {
+      this.selectedDays = [moment(), moment().add(1, 'day'), moment().add(2, 'day')];
+    }
+    this.displayedMonth = this.selectedDays[0].get("month");
+    this.displayedYear = this.selectedDays[0].get("year");
+    this.month = this.getMonthArray(this.displayedYear, this.displayedMonth + 1);
+  }
+
+  getNextMonth(){
+    if (this.displayedMonth == 11){
+      this.displayedMonth = 0;
+      this.displayedYear++;
+    } else {
+      this.displayedMonth++;
+    }
+
+    this.month = this.getMonthArray(this.displayedYear, this.displayedMonth + 1);
+    console.log(this.month);
+  }
+
+  getPreviousMonth(){
+    if (this.displayedMonth == 0){
+      this.displayedMonth = 11;
+      this.displayedYear--;
+    } else {
+      this.displayedMonth--;
+    }
+
+    this.month = this.getMonthArray(this.displayedYear, this.displayedMonth + 1);
+  }
+
+  getMonthArray(year, month) {
+    var date = new Date(year, month-1, 1);
+    var result = [];
+    var preFill = [], postFill = [], tmp = [new Date(date.getTime())];
+
+    while (date.getDay() < 6) {
+      date.setDate(date.getDate() + 1);
+      tmp.push(new Date(date.getTime()));
+    }
+
+    for (var i = 0; i < 7 - tmp.length; i++){
+      preFill.push(null);
+    }
+
+    result.push(preFill.concat(tmp));
+
+    tmp = [];
+
+    while (date.getMonth() == month-1) {
+      date.setDate(date.getDate() + 1);
+
+      if (date.getMonth() == month-1){
+        tmp.push(new Date(date.getTime()));
+        if (date.getDay() == 6) {
+          result.push(tmp);
+          tmp = [];
+        }
+      }
+    }
+
+    for (var i = 0; i < 7 - tmp.length; i++){
+      postFill.push(null);
+    }
+
+    result.push(tmp.concat(postFill));
+
+    return result;
   }
 }
